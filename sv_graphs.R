bff908eac81e14143f745d966fbe7e669a7e30a6
sversh <sofia@mainstreetgenome.com>
2015-02-24 19:35:52 -0500

    Bar graphs added

--- sv_graphs.R
+++ sv_graphs.R
@@ -27,8 +27,9 @@ df_inspect$A_grade[df_inspect$GRADE == "A"] <- 1
 df_inspect$B_grade[df_inspect$GRADE == "B"] <- 1
 df_inspect$C_grade[df_inspect$GRADE == "C"] <- 1
 
-
+###############################
 ######Time series of grades
+################################
 #Collapse counts by year/month
 
 monthyear<-group_by(df_inspect, year, month)
@@ -61,7 +62,7 @@ d4 <- counts_by_month[, c("date", "perc_C")]
 
 
 #Plot the total number of inspections
-a<-ggplot() + 
+graph_a<-ggplot() + 
   geom_line(data = d1, aes(x = date, y = total_count)) +
   xlab('Date') +
   ylab('Total number of inspections') + 
@@ -70,7 +71,7 @@ a<-ggplot() +
 
 #Plot share of inspections by grade
 dd_sub = counts_by_month[,c("date", "perc_A", "perc_B", "perc_C")]
-b<-ggplot(data = counts_by_month, aes(x = date)) +
+graph_b<-ggplot(data = counts_by_month, aes(x = date)) +
   geom_line(aes(y = perc_A, colour = "A grade")) +
   geom_line(aes(y = perc_B, colour = "B grade")) +
   geom_line(aes(y = perc_C, colour = "C grade")) +
@@ -82,9 +83,62 @@ b<-ggplot(data = counts_by_month, aes(x = date)) +
   labs(title="Share of inspection results by grade over time")
 
 
+
+####################################
+#Get number of violations by category
+####################################
+# read in the classifications file
+classifications <- read.csv(file = "Classifications.csv")
+
+data_class <- merge(data, classifications, by="VIOLATION.CODE")
+#data_class_selected <-data[,c("VIOLATION.DESCRIPTION", "CLASSIFICATION")]
+
+## set the levels in order we want
+data_class <- within(data_class, 
+                   CLASSIFICATION <- factor(CLASSIFICATION, 
+                                      levels=names(sort(table(CLASSIFICATION), 
+                                                        increasing=TRUE))))
+## plot
+graph_d <-ggplot(data_class,aes(x=CLASSIFICATION))+geom_bar(binwidth=1, fill="blue") + 
+  coord_flip() +
+  labs(y = "Number of violations") +
+  labs(x = "Violation category") +
+  ggtitle("Number of violations by category, 2012-2014")  +
+  theme(axis.title.x = element_text(size = rel(0.7)), axis.title.x = element_text(size = rel(0.7)))
+
+####################################
+#Top violations for c-grade restaurants
+####################################
+#Keep only the C-violations
+data_C<-data[data$GRADE=='C',]
+
+#Keep only the top 10 violations
+data_C_collapsed <- data.frame(table(data_C$VIOLATION.DESCRIPTION))
+data_C_collapsed <- data_C_collapsed[order(-data_C_collapsed$Freq),]
+data_C_collapsed_top10 <- data_C_collapsed[1:10, ]
+
+#Take the first sentence (or everything before first semicolon) from every description
+data_C_collapsed_top10$short <- sapply(strsplit(as.character(data_C_collapsed_top10$Var1), "\\."), `[`, 1)
+data_C_collapsed_top10$short <- sapply(strsplit(as.character(data_C_collapsed_top10$short), ";"), `[`, 1)
+
+#Put in line breaks at 45th character to display nicely
+data_C_collapsed_top10$split <- gsub('(.{1,45})(\\s|$)', '\\1\n', data_C_collapsed_top10$short)
+
+#Plot
+graph_e<-ggplot(data_C_collapsed_top10,aes(x=split, y=Freq))+geom_bar(stat="identity", fill="blue") + 
+  coord_flip() +
+  labs(y = "Number of violations") +
+  labs(x = "Violation category") +
+  ggtitle("Top violations for C-graded inspections")  +
+  theme(axis.title.x = element_text(size = rel(0.7)), axis.title.x = element_text(size = rel(0.7)))
+
+
+
 #Print to PDF
 pdf(file = "sv_plots.pdf", width = 11, height = 8.5)
 print(a)
 print(b)
+print(graph_d)
+print(graph_e)
 dev.off()
 
